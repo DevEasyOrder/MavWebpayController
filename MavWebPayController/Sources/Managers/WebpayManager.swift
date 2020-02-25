0eae6e93632cffea8c09612451eab7c873e5cc22
Fernando Jorquera <fernando.jorquera@mavericks.cool>
2020-02-25 16:51:32 -0300

    alamofire 5

--- MavWebPayController/Sources/Managers/WebpayManager.swift
+++ MavWebPayController/Sources/Managers/WebpayManager.swift
@@ -20,16 +20,16 @@ public class WebpayManager {
     public func enrolleCard()->Promise<String>{
         return Promise<String>(work: {
             fulfill,reject in
-            Alamofire.request(URLsManager.Router.initInscription(params: nil)).responseJSON(completionHandler: {
+            AF.request(URLsManager.Router.initInscription(params: nil)).responseJSON(completionHandler: {
                 response in
                 
-                guard response.result.error == nil else{
-                    reject(response.result.error!)
+                guard response.error == nil else{
+                    reject(response.error!)
                     return
                 }
                 
-                print(response.result.value)
-                if let dict = response.result.value as? [String : Any]{
+                print(response.value)
+                if let dict = response.value as? [String : Any]{
                     if let url = dict["data"] as? String{
                         fulfill(url)
                         return
@@ -43,15 +43,15 @@ public class WebpayManager {
     public func finishEnrollement()->Promise<[String : Any]>{
         return Promise<[String : Any]>(work: {
             fulfill,reject in
-            Alamofire.request(URLsManager.Router.finishInscription(params: nil)).responseJSON(completionHandler: {
+            AF.request(URLsManager.Router.finishInscription(params: nil)).responseJSON(completionHandler: {
                 response in
                 
-                guard response.result.error == nil else{
-                    reject(response.result.error!)
+                guard response.error == nil else{
+                    reject(response.error!)
                     return
                 }
                 
-                if let dict = response.result.value as? [String : Any]{
+                if let dict = response.value as? [String : Any]{
                     if let url = dict["data"] as? [String : Any]{
                         let code = url["responseCode"] as? Int ?? 0
                         guard code == 0 else{
@@ -71,14 +71,13 @@ public class WebpayManager {
     public func unsubscribeEnrollment()->Promise<Bool>{
         return Promise<Bool>(work: {
             fulfill, reject in
-            Alamofire.request(URLsManager.Router.unsubscribe(params: nil)).responseJSON(completionHandler: {
+            AF.request(URLsManager.Router.unsubscribe(params: nil)).responseJSON(completionHandler: {
                 response in
-                guard response.result.error == nil else{
-                    reject(response.result.error!)
+                guard response.error == nil else{
+                    reject(response.error!)
                     return
                 }
-                
-                if let dict = response.result.value as? [String : Any]{
+                if let dict = response.value as? [String : Any]{
                     if let url = dict["data"] as? Bool{
                         fulfill(url)
                         return
@@ -99,16 +98,16 @@ public class WebpayManager {
                 queryParams = queryParams.appending("\(key)=\(String(describing: MavWebpayConfiguration.shared.authorizationParams[key]))&")
             }
             queryParams = String(queryParams.dropLast())
-            Alamofire.request("\(MavWebpayConfiguration.shared.baseURL)/authorize?\(queryParams)", method: .post, parameters: nil, headers: [
+            AF.request("\(MavWebpayConfiguration.shared.baseURL)/authorize?\(queryParams)", method: .post, parameters: nil, headers: [
                 "Authorization" : "Bearer \(MavWebpayConfiguration.shared.token)"
                 ]).responseJSON(completionHandler: {
                     response in
-                    guard response.result.error == nil else{
-                        reject(response.result.error!)
+                    guard response.error == nil else{
+                        reject(response.error!)
                         return
                     }
                     
-                    if let dict = response.result.value as? [String : Any]{
+                    if let dict = response.value as? [String : Any]{
                         if let url = dict["data"] as? [String : Any]{
                             fulfill(url)
                             return
